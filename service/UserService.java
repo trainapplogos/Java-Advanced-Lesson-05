edda0e7d71384d2995be04f6ba24d94b285a926b
trainapp <75678105+trainapplogos@users.noreply.github.com>
2021-06-17 00:53:12 +0300

    JA-Lesson-05 commit

---
+++ service/UserService.java
@@ -0,0 +1,8 @@
+package ua.lviv.trainapplogos.service;
+
+import ua.lviv.trainapplogos.domain.User;
+import ua.lviv.trainapplogos.shared.AbstractCRUD;
+
+public interface UserService extends AbstractCRUD<User> {
+
+}
